9c1e4dc39f3804a8c2932aea30bf96d3308ff9d6
Hrk01221 <hrkisdead01221@gmail.com>
2026-07-31 17:55:48 +0600

    done

--- src/sections/Achievements.jsx
+++ src/sections/Achievements.jsx
@@ -15,7 +15,7 @@ const achievements = [
   {
     img: "https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/480401934_122199339740182066_2937126468643158199_n.jpg?stp=dst-jpg_tt6&cstp=mx1080x1080&ctp=s1080x1080&_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHLEvh2a3Bo1lPJC_cVzkLHSscqT4Zd4VBKxypPhl3hUOg9VGmvTd91pzVltLEaNW0uQv5kXinTtcg7WAbL3erb&_nc_ohc=CiZPfyDEZJsQ7kNvwEBO-Re&_nc_oc=AdrsQ5HLm2W_iiRZVL4prk455jxDfpFWRzXEMR5nZua7FOXVpvtzxqwnr1wi5_72NOE&_nc_zt=23&_nc_ht=scontent.fdac138-2.fna&_nc_gid=kg1gdnTTJQD9E7SahSw_lA&_nc_ss=7b2a8&oh=00_AQG3tlAuNHqHLPWqpnt8mBZ3c7IcR7mTodvQHnjHCuH9lA&oe=6A722B16",
     type: "Programming Contest",
-    name: "AUST CSE CARNIBVAL 6.0 | IAPC",
+    name: "AUST CSE CARNIVAL 6.0 | IAPC",
     rank: "Champion",
     team: "AUST_FORCEDCODERS",
     solved: "6 Problems",
@@ -24,7 +24,7 @@ const achievements = [
   {
     img: "https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-6/468223187_122185012718182066_4323535638077374401_n.jpg?stp=dst-jpg_tt6&cstp=mx2048x1365&ctp=s2048x1365&_nc_cat=103&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGEs4-ij_0ImZz3v03VT2jmKKMrIY59sWMooyshjn2xY8ftl2EzpD2NaAGz7cugSqCHiEwknYj3cxjMEGWUEG-e&_nc_ohc=bG-mVdBZ7DYQ7kNvwHp3-Ak&_nc_oc=AdqXuntnv23LdYWaLQrRRwNQrvwdW5Lx8cMdKfCmWP6QZGBLTWgmk4zcRRk_j8REBXI&_nc_zt=23&_nc_ht=scontent.fdac138-2.fna&_nc_gid=hUgu4HtTqBiu7e3iG3amEQ&_nc_ss=7b2a8&oh=00_AQFe3o87YQPP4iVM9PuGkMu8BXpAdu_o_ztsZDoVPWTxtA&oe=6A720F2D",
     type: "Programming Contest",
-    name: "AUST CSE CARNIBVAL 1.0 | IAPC",
+    name: "AUST CSE CARNIVAL 1.0 | IAPC",
     rank: "Champion - Junior Segment",
     team: "AUST_CODECRUSADERS",
     solved: "2 Problems",
